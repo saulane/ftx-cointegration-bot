7f5926046031fd6223efd190474e18d14b24b0b9
Saulane <rave.simon@live.fr>
2021-05-20 02:44:05 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,22 @@ mod auth;
 mod ws;
 mod sign;
 
-const API_KEY: &str = "Lo5C3gwBRYav_-hECTlY2ej85iwkegqYEJU24Ip-";
-const API_SECRET: &str = "Yvek0WTrOw03QQYbP4B-ZQ2S_FZoNXExtfCGYrAf";
+const API_KEY: &str = "Wij-a7W4DTm0Ly_5vJqu9DZrhRdzG_ma4OuAeMAP";
+const API_SECRET: &str = "JtPh-moNuFE5eyRvqA3fFfBqMPEFk7ix3B-mc5-E";
 
 #[tokio::main]
 async fn main() {
 
 
-    println!("{:?}",sign::signature("GET/api/markets").unwrap());
+    //println!("{:?}",sign::signature("T4lPid48QtjNxjLUFOcUZghD7CUJ7sTVsfuvQZF2","/api/markets", 1588591511721, "GET").unwrap());
 
     let api_client = reqwest::Client::new();
-    let test = auth::get_jsoned_data(api_client, "BTC-PERP", "300").await.unwrap();
-    println!("{:?}", test);
 
+    // let test = auth::get_jsoned_data(api_client, "BTC-PERP", "300").await.unwrap();
+    // println!("{:?}", test);
+
+    let test_balance = auth::get_balance(api_client, API_KEY, API_SECRET).await.unwrap();
+    println!("{:?}", test_balance);
 
 
     // let (mut socket, response) =
--- src/sign.rs
+++ src/sign.rs
@@ -4,28 +4,28 @@ use std::time::{SystemTime};
 use sha2::Sha256;
 use hmac::{Hmac, Mac, NewMac};
 
-type HmacSha256 = Hmac<Sha256>;
 
-pub fn current_ts() -> u64 {
-    let ts = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
-
+pub fn current_ts() -> u128 {
+    let ts = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_millis();
     ts
 }
 
 
-pub fn signature(endpoint: &str) -> Result<String, ()> {
-    let ts: String = current_ts().to_string();
+pub fn signature(api_secret: &str, time: u128, endpoint: &str, method: &str) -> Result<String, ()> {
+    type HmacSha256 = Hmac<Sha256>;
+    let ts: String = time.to_string();
 
-    let message = format!("{}{}", ts, endpoint);
+    let message = format!("{}{}{}", ts, method, endpoint);
+    println!("Sign Payload: {}", message);
 
-
-    let secret = "T4lPid48QtjNxjLUFOcUZghD7CUJ7sTVsfuvQZF2";
-
-    let mut mac = HmacSha256::new_from_slice(&secret.as_bytes()).expect("Problem keying the API_SECRET");
+    let mut mac = HmacSha256::new_from_slice(api_secret.as_bytes()).expect("Problem keying the API_SECRET");
     mac.update(&message.as_bytes());
     let result = mac.finalize().into_bytes();
 
     let r2 = hex::encode(&result);
+    
+    // println!("{}",message);
+    // println!("{}",r2);
 
     Ok(r2)
 }
